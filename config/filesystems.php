43b4256bfe5462414071e394dc7c86b800fa354a
yanntyb <yanntyb.lbc@gmail.com>
2023-01-10 23:56:17 +0100

    link

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,7 +71,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        base_path('test') => storage_path('app/test'),
+        public_path('test') => storage_path('app/test'),
     ],
 
 ];
