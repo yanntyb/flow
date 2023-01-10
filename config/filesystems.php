e1330767531268fd83d0ff6a3e4127dabb592f35
yanntyb <yanntyb.lbc@gmail.com>
2023-01-10 21:59:47 +0100

    change link

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,7 +71,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        public_path('test') => storage_path('app/test'),
+        base_path('test') => storage_path('app/test'),
     ],
 
 ];
